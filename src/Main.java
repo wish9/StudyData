15183b55a0475e57f03e51d219d169a5ac07cd33
wish9 <wjwee9@gmail.com>
2023-01-16 01:13:20 +0900

    Stack형 자료구조 직접만들기

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,21 @@ public class Main {
     public static void main(String[] args) {
 
         //Stack형 자료구조 연습
-        Stacktest stacktest = new Stacktest();
-        stacktest.printStack();
+        Stacktest stack = new Stacktest();
+        //stack.printStack();
+
+        //Stack형 자료구조 직접만들기
+        stack.size();
+        for(int i = 1; i < 15; i++) {
+            stack.push(i);
+        }
+        stack.pop(); // 14지우기
+        stack.pop(); // 13지우기
+        stack.size(); // 길이 = 12
+        stack.push(77); // 77추가하기
+        stack.size(); // 길이 = 13
+
+        System.out.println(stack.show());
+
     }
 }
--- src/Stacktest.java
+++ src/Stacktest.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Stack;
 
 public class Stacktest {
@@ -21,4 +22,35 @@ public class Stacktest {
     public void print(Object a) {
         System.out.println(a);
     }
+
+    private ArrayList<Integer> listStack = new ArrayList<Integer>();
+
+    public void push(Integer data) {
+        listStack.add(data);
+    }
+
+    public Integer pop() {
+        if(listStack.size()==0) {
+            return null;
+        }else {
+            return listStack.remove(listStack.size()-1);
+        }
+    }
+
+    public int size() {
+        return listStack.size();
+    }
+
+    public Integer peek() {
+        if(listStack.size()==0) {
+            return null;
+        }else {
+            return listStack.get(listStack.size()-1);
+        }
+    }
+
+    public String show() {
+        return listStack.toString();
+    }
+    public void clear() { listStack.clear(); }
 }
