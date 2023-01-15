7472c61890a3a0e0e724067d32300f28cc4d10d8
wish9 <wjwee9@gmail.com>
2023-01-16 01:52:45 +0900

    Queue형 자료구조 직접만들기

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,38 @@ public class Main {
     public static void main(String[] args) {
 
         //Stack형 자료구조 연습
-        Stacktest stack = new Stacktest();
+//        Stacktest stack = new Stacktest();
         //stack.printStack();
 
         //Stack형 자료구조 직접만들기
-        stack.size();
-        for(int i = 1; i < 15; i++) {
-            stack.push(i);
-        }
-        stack.pop(); // 14지우기
-        stack.pop(); // 13지우기
-        stack.size(); // 길이 = 12
-        stack.push(77); // 77추가하기
-        stack.size(); // 길이 = 13
+//        stack.size();
+//        for(int i = 1; i < 15; i++) {
+//            stack.push(i);
+//        }
+//        stack.pop(); // 14지우기
+//        stack.pop(); // 13지우기
+//        stack.size(); // 길이 = 12
+//        stack.push(77); // 77추가하기
+//        stack.size(); // 길이 = 13
+//
+//        System.out.println(stack.show());
 
-        System.out.println(stack.show());
+        QueueTest queue = new QueueTest();
+
+        queue.size(); // 0
+        for(int i = 1; i <= 10; i++) {
+            queue.add(i);
+        }
+        queue.poll(); // 1
+        queue.poll(); // 2
+        queue.size(); // 8
+        queue.add(77);
+        queue.size(); // 9
+        queue.poll(); // 3
+        queue.poll(); // 4
+        queue.size(); // 7
+
+        System.out.println(queue.show());
 
     }
 }
---
+++ src/QueueTest.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class QueueTest {
+    private ArrayList<Integer> listQueue = new ArrayList<Integer>();
+
+    public void add(Integer data) { // 큐에 데이터 추가
+        listQueue.add(data);
+    }
+
+    public Integer poll() { // 가장 먼저 추가된 데이터 삭제, 리턴
+        if(listQueue.size()==0) {
+            return null;
+        }else {
+            return listQueue.remove(0);
+        }
+    }
+
+    public int size() { // 큐에 저장된 데이터 크기 리턴
+        return listQueue.size();
+    }
+
+    public Integer peek() { // 가장 먼저 추가된 데이터 리턴
+        if(listQueue.size()==0) {
+            return null;
+        }else {
+            return listQueue.get(0);
+        }
+    }
+
+    public String show() {
+        return listQueue.toString();
+    }
+
+    public void clear() {
+        listQueue.clear();
+    }
+}
