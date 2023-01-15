5540ba30b0a37109adfcd9f7b09cf232be9ce9b8
wish9 <wjwee9@gmail.com>
2023-01-16 00:39:01 +0900

    Stack형 자료구조 연습

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Stacktest stacktest = new Stacktest();
+        stacktest.printStack();
+    }
+}
