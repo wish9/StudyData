a70b2305f87845ddde9df55f083f31601f0fa089
wish9 <wjwee9@gmail.com>
2023-01-16 00:41:04 +0900

    Stack형 자료구조 연습

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Stack;
 public class Main {
     public static void main(String[] args) {
 
+        //Stack형 자료구조 연습
         Stacktest stacktest = new Stacktest();
         stacktest.printStack();
     }
--- src/Stacktest.java
+++ src/Stacktest.java
@@ -2,13 +2,15 @@ import java.util.Stack;
 
 public class Stacktest {
 
-    public void printStack() {
+    public void printStack() { //Stack형 자료구조 연습
         Stack<Integer> stack = new java.util.Stack<>(); //Integer형 스택 선언
 
         print(stack.push(1));
         print(stack.push(2));
         print(stack.push(3));
         print(stack.push(4));
+
+        print(stack);
 
         print(stack.pop());
         print(stack.pop());
