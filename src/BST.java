3a71d08b3a443a008c789b8a931df2ef895c6c7f
wish9 <wjwee9@gmail.com>
2023-01-22 22:01:24 +0900

    자료구조 - BST구현

---
+++ src/BST.java
@@ -0,0 +1,126 @@
+import java.util.ArrayList;
+
+public class BST {
+    public static class Node { // 트리를 구성하는 노드 클래스
+        private int data;
+        private Node left;
+        private Node right;
+
+        public Node(int data) { //생성자
+            this.setData(data);
+            setLeft(null);
+            setRight(null);
+        }
+
+        public int getData() {
+            return data;
+        }
+
+        public Node getLeft() {
+            return left;
+        }
+
+        public Node getRight() {
+            return right;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public void setLeft(Node left) {
+            this.left = left;
+        }
+
+        public void setRight(Node right) {
+            this.right = right;
+        }
+    }
+
+    public static class binarySearchTree { // 이진 탐색 트리의 클래스
+        public Node root;
+
+        public binarySearchTree(){
+            root = null;
+        }
+
+        public void insert(int data) { // tree에 value를 추가하는 메서드
+            Node newNode = new Node(data); // 왼쪽, 오른쪽 자식 노드가 null 이며 data 의 값이 저장된 새 노드 생성
+            if (root == null) {// 루트 노드가 없을때, 즉 트리가 비어있는 상태일 때,
+                root = newNode;
+                return;
+            }
+            if(root.data == data) return;   //중복일때 정지
+
+            Node currentNode = root;
+            Node parentNode = null;
+
+            while (true) {
+                parentNode = currentNode;
+
+                if (data < currentNode.getData()) { // 해당 노드보다 작을 경우
+                    currentNode = currentNode.getLeft();
+                    if (currentNode == null) {
+                        parentNode.setLeft(newNode);
+                        return;
+                    }else if(currentNode.data == newNode.data) return;
+                } else { // 해당 노드보다 클 경우
+                    currentNode = currentNode.getRight();
+                    if (currentNode == null) {
+                        parentNode.setRight(newNode);
+                        return;
+                    }else if(currentNode.data == newNode.data) return;
+                }
+            }
+        }
+
+        // tree의 value값을 탐색
+        public boolean contains(int data) {
+            Node currentNode = root;
+            while (currentNode != null) {
+                // 찾는 value값이 노드의 value와 일치한다면, true를 리턴합니다.
+                if (currentNode.data == data) {
+                    return true;
+                }
+
+                if (currentNode.data > data) {
+                    // 찾는 value값이 노드의 value 보다 작다면, 현재 노드를 left로 변경후 다시 반복
+                    currentNode = currentNode.left;
+                }else {
+                    // 찾는 value값이 노드의 value 보다 크다면, 현재 노드를 right로 변경후 다시 반복
+                    currentNode = currentNode.right;
+                }
+            }
+            return false;
+        }
+
+
+        // 이진 탐색 트리를 순회하는 메서드
+        public ArrayList<Integer> preorderTree(Node root, int depth, ArrayList<Integer> list) {    //전위 순회
+            if (root != null) {
+                list.add(root.getData());
+                preorderTree(root.getLeft(), depth + 1, list);
+                preorderTree(root.getRight(), depth + 1, list);
+            }
+            return list;
+        }
+
+        public ArrayList<Integer> inorderTree(Node root, int depth, ArrayList<Integer> list) { //중위 순회
+            if (root != null) {
+                inorderTree(root.getLeft(), depth + 1, list);
+                list.add(root.getData());
+                inorderTree(root.getRight(), depth + 1, list);
+            }
+            return list;
+        }
+
+        public ArrayList<Integer> postorderTree(Node root, int depth, ArrayList<Integer> list) {   //후위 순회
+            if (root != null) {
+                postorderTree(root.getLeft(), depth + 1, list);
+                postorderTree(root.getRight(), depth + 1, list);
+                list.add(root.getData());
+            }
+            return list;
+        }
+    }
+}
