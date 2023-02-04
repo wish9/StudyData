a9853be68c47c84ecf143e0cdb9fd8116c47ea4b
wish9 <wjwee9@gmail.com>
2023-02-05 02:38:23 +0900

    의존성 주입(DI) 예시 - new 사용 (stub버전)

--- src/Spring/DI/StoreClient.java
+++ src/Spring/DI/StoreClient.java
@@ -2,7 +2,7 @@ package Spring.DI;
 import java.util.*;
 public class StoreClient {
     public static void main(String[] args) {
-        MenuService menuService = new MenuService();
+        MenuServiceStub menuService = new MenuServiceStub(); // MenuService -> MenuServiceStub
         MenuControl menuControl = new MenuControl(menuService);
         List<Menu> menuList = menuControl.getMenus();
         System.out.println(menuList);
