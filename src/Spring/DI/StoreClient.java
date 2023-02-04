7a9bd34ca9a61e7ab06d98aaf19f08a53118101b
wish9 <wjwee9@gmail.com>
2023-02-05 02:24:26 +0900

    의존성 주입(DI) 예시 - new 사용

--- src/Spring/DI/StoreClient.java
+++ src/Spring/DI/StoreClient.java
@@ -1,12 +1,11 @@
 package Spring.DI;
-
-import java.awt.*;
-
+import java.util.*;
 public class StoreClient {
     public static void main(String[] args) {
         MenuService menuService = new MenuService();
         MenuControl menuControl = new MenuControl(menuService);
-//        List<Menu> menuList = menuControl.getMenus();
+        List<Menu> menuList = menuControl.getMenus();
+        System.out.println(menuList);
     }
 
 }
